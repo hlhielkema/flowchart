df03f3b008739388e321c69a33206605f79a044c
Hielke <hielkehielkema93@gmail.com>
2020-12-27 16:16:15 +0100

    implemented rename label and remove connection; action hover styles added

--- src/scripts/Flowchart.js
+++ src/scripts/Flowchart.js
@@ -12,6 +12,14 @@ function Flowchart(selector) {
     this.nodes = new NodeCollection();
     this.canvas = new CanvasController(this.container, this.nodes);
     this.dom = new DomController(this, this.nodes);
+
+    // Create default rename handler
+    this.renameLabelHandler = function(currentName, rename) {
+        var newName = prompt('Enter a new name', currentName);
+        if (newName !== null) {
+            rename(newName);
+        }
+    }    
 }
 
 Flowchart.prototype.init = function () {
@@ -32,11 +40,33 @@ Flowchart.prototype.onNodePositionChanged = function() {
 }
 
 Flowchart.prototype.onRenameLabel = function(from, to) {
-    alert('rename label: ' + from.id + '->' + to.id);
+    var connection = null;
+    for (var i = 0; i < from.connections.length; i++) {
+        if (from.connections[i].id === to.id) {
+            connection = from.connections[i];
+        }
+    }
+    if (connection === null) {
+        throw 'Connection not found';
+    }
+
+    const self = this;
+    this.renameLabelHandler(connection.label, function(newName) { 
+        if (newName.length > 0 && connection.label !== newName) {                
+            connection.label = newName;
+            self.render();
+        }
+     });
 }
 
 Flowchart.prototype.onRemoveConnection = function(from, to) {
-    alert('remove connection: ' + from.id + '->' + to.id);
+    for (var i = 0; i < from.connections.length; i++) {
+        if (from.connections[i].id === to.id) {
+            from.connections.splice(i, 1);
+            break;
+        }
+    }    
+    this.render();
 }
 
 Flowchart.prototype.onEditNode = function(nodeId) {
